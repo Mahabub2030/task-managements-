1240fb87bc1f6d22a7f89a1c2a9218d5969116c4
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-12 18:33:29 +0300

    added clx for styling

--- src/components/pages/Tasks.tsx
+++ src/components/pages/Tasks.tsx
@@ -1,18 +1,22 @@
-import { selectFilter, selectTasks } from "@/features/tasks/taskSlice"
-import { useAppSelector } from "@/hooks/hooks"
-
+import { selectTasks } from "@/features/tasks/taskSlice";
+import { useAppSelector } from "@/hooks/hooks";
+import TaskCard from "../modules/Task/TaskCard";
 
 export default function Tasks() {
-
-  const tasks = useAppSelector(selectTasks)
-  const filter = useAppSelector(selectFilter)
-  console.log(tasks,filter)
-
-
+  const tasks = useAppSelector(selectTasks);
+  // const filter = useAppSelector(selectFilter)
+  console.log(tasks);
+  // console.log(filter)
 
   return (
-    <div>
+    <div className="mx-auto max-w-7xl px-5 mt-20">
       <h1 className="text-xl">Task welcom to the pages</h1>
+
+      <div className="space-y-5 mt-5">
+        {tasks.map((task) => (
+          <TaskCard task= {task} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
--- src/features/tasks/taskSlice.ts
+++ src/features/tasks/taskSlice.ts
@@ -2,39 +2,52 @@ import type { RootState } from "@/store";
 import type { ITask } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
 interface InitialSate {
-    tasks:ITask[];
-    filter:"all"|"high"|"low"
+  tasks: ITask[];
+  filter: "all" | "high" | "low";
 }
 
-const initialState : InitialSate ={
-    tasks:[
-        {
-        id:"14gggggg444444444",
-        title:"initialisce the page",
-        description:"create initial scile",
-        dueDate:"2025-12",
-        isCopleted:false,
-        priority:"High"
-    }
-    ],
-    filter:"all"
-}
+const initialState: InitialSate = {
+  tasks: [
+    {
+      id: "14gggggg444444444",
+      title: "initialisce the page",
+      description: " initial scile",
+      dueDate: "2025-12",
+      isCopleted: false,
+      priority: "High",
+    },
+    {
+      id: "14gggggg444444444",
+      title: "create Tasks page",
+      description: "create Tasks scile",
+      dueDate: "2025-12",
+      isCopleted: false,
+      priority: "Low",
+    },
+    {
+      id: "14gggggg444444444",
+      title: "create Tasks page",
+      description: "create Tasks scile",
+      dueDate: "2025-12",
+      isCopleted: false,
+      priority: "Medium",
+    },
+  ],
+  filter: "all",
+};
 
 const taskSlice = createSlice({
-    name:"task",
-    initialState,
-    reducers:{}
+  name: "task",
+  initialState,
+  reducers: {},
+});
 
-})
-
-export const selectTasks = (state:RootState) =>{
-    return state.todo.tasks
-}
-export const selectFilter = (state:RootState) =>{
-    return state.todo.filter
-}
+export const selectTasks = (state: RootState) => {
+  return state.todo.tasks;
+};
+export const selectFilter = (state: RootState) => {
+  return state.todo.filter;
+};
 
 export default taskSlice.reducer;
